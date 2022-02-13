fdabccd27c3bcd1705bf150bcb65228314c9e6e6
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2022-02-13 15:29:24 +0100

    allowing x-auth-token header for CORS plugin to see in response in client

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
-const _ = require('lodash');
-const { User, validate } = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const _ = require('lodash')
+const { User, validate } = require('../models/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 //const config = require('config');
 //nconst auth = require('../middleware/auth');
 
 router.get('/me', async function (req, res) {
-  const user = await User.findById(req.user._id).select('-password');
-  res.send(user);
-});
+  const user = await User.findById(req.user._id).select('-password')
+  res.send(user)
+})
 
 router.get('/', async function (req, res) {
-  const users = await User.find().sort('id');
-  return res.send(users);
-});
+  const users = await User.find().sort('id')
+  return res.send(users)
+})
 
 router.post('/', async function (req, res) {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  const { error } = validate(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
-  let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('This user is already registered');
+  let user = await User.findOne({ email: req.body.email })
+  if (user) return res.status(400).send('This user is already registered')
 
-  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  user = new User(_.pick(req.body, ['name', 'email', 'password']))
   try {
-    // hashing passwird
-    const salt = await bcrypt.genSalt();
-    user.password = await bcrypt.hash(user.password, salt);
-    await user.save();
-    const token = user.generateAuthToken();
+    // hashing password
+    const salt = await bcrypt.genSalt()
+    user.password = await bcrypt.hash(user.password, salt)
+    await user.save()
+    const token = user.generateAuthToken()
     res
       .header('x-auth-token', token)
-      .send(_.pick(user, ['_id', 'name', 'email']));
+      .send(_.pick(user, ['_id', 'name', 'email']))
   } catch (err) {
-    console.log('Error while saving user to DB...', err);
-    return res.status(500);
+    console.log('Error while saving user to DB...', err)
+    return res.status(500)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
--- startup/routes.js
+++ startup/routes.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const error = require('../middleware/error');
+const express = require('express')
+const error = require('../middleware/error')
 
-const cors = require('cors');
+const cors = require('cors')
+const corsOptions = {
+  exposedHeaders: ['Authorization', 'x-auth-token'],
+}
 // requiring routes
-const users = require('../routes/users');
-const categories = require('../routes/categories');
-const taskTypes = require('../routes/taskTypes');
-const activityTypes = require('../routes/activityTypes');
-const activities = require('../routes/activities');
+const users = require('../routes/users')
+const categories = require('../routes/categories')
+const taskTypes = require('../routes/taskTypes')
+const activityTypes = require('../routes/activityTypes')
+const activities = require('../routes/activities')
 
 // adding all the routes
 module.exports = function (app) {
-  app.use(cors());
+  app.use(cors(corsOptions))
 
-  app.use(express.json());
-  app.use('/api/users', users);
-  app.use('/api/categories', categories);
-  app.use('/api/taskTypes', taskTypes);
-  app.use('/api/activityTypes', activityTypes);
-  app.use('/api/activities', activities);
+  app.use(express.json())
+  app.use('/api/users', users)
+  app.use('/api/categories', categories)
+  app.use('/api/taskTypes', taskTypes)
+  app.use('/api/activityTypes', activityTypes)
+  app.use('/api/activities', activities)
 
   // error handling in middleware function
-  app.use(error);
-};
+  app.use(error)
+}
