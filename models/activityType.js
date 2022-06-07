92dbac3d41bd5afefe8fa9194eb85a3b2b66fb10
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2022-06-07 09:31:27 +0100

    change rewards validation

--- models/activityType.js
+++ models/activityType.js
@@ -1,30 +1,30 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi)
 
-const mongoose = require('mongoose');
-const { TaskType } = require('./taskType');
+const mongoose = require('mongoose')
+const { TaskType } = require('./taskType')
 
 const schema = {
-  reward: { type: Number, required: true, min: -1, max: 2 },
+  reward: { type: Number, required: true, min: 0, max: 100 },
   tasktype: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'TaskType',
     required: true,
   },
   description: { type: String, required: true },
-};
+}
 
-const object = new mongoose.model('activityType', schema);
+const object = new mongoose.model('activityType', schema)
 
 function validate(object) {
   const schema = Joi.object({
     reward: Joi.number().min(-1).max(2).required(),
     tasktype: Joi.objectId().required(),
     description: Joi.string().required(),
-  });
-  return schema.validate(object);
+  })
+  return schema.validate(object)
 }
 
-module.exports.ActivityType = object;
-module.exports.validate = validate;
-module.exports.ActivityTypeSchema = schema;
+module.exports.ActivityType = object
+module.exports.validate = validate
+module.exports.ActivityTypeSchema = schema
